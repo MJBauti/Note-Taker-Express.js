be4cd5432e991171ed23e7b4994da78a5b6a158a
MJBauti <michael.bautista@yahoo.com>
2022-09-20 22:34:02 -0700

    implemented a helper fsUtils.js

--- routes/noteTaker.js
+++ routes/noteTaker.js
@@ -1,27 +1,22 @@
 const noteTaker = require('express').Router();
-const fs = require("fs");
-const notes = require("../db/db.json");
-const path = require("path");
 const { v4: uuidv4 } = require('uuid');
+const { createNewNote, updateDb } = require('../helpers/fsUtils');
+const {notes} = require("../../db/db.json");
 
 noteTaker.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../db/db.json"))
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data))
 });
 
 noteTaker.post("/", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("../db/db.json"));
-    const newNotes = req.body;
-    newNotes.id = uuidv4();
-    notes.push(newNotes);
-    fs.writeFileSync("../db/db.json", JSON.stringify(notes))
-    res.json(notes);
+    req.body.id = uuidv4();
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
 });
 
 noteTaker.delete("/:id", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("../db/db.json"));
-    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
-    fs.writeFileSync("../db/db.json", JSON.stringify(delNote));
-    res.json(delNote);
+    const params = req.params.id
+    updateDb(params, notes);
+    res.redirect('');
 })
 
 module.exports = noteTaker
